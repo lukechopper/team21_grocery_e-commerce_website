f67875e812bcfb1e5125e420f6e88d96b6ddb64a
lukechopper <lukechopper22@gmail.com>
2022-11-13 15:49:07 +0000

    Fixed a minor but fatal error to do with the naming of a certain view: Signup.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
     }
 
     public function accessSignup(){
-        $returnVal = $this->preventUnLoggedInUserFromReachingTheRouteUnlessTheyHaveJustBeenFlashedTheRelevantSessionVariables('signup');
+        $returnVal = $this->preventUnLoggedInUserFromReachingTheRouteUnlessTheyHaveJustBeenFlashedTheRelevantSessionVariables('Signup');
         return $returnVal;
     }
 
