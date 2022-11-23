ddf473ff939a367aa2d476c8b1863f4e029678a7
lukechopper <lukechopper22@gmail.com>
2022-11-23 10:25:32 +0000

    Save a backup of the project close enough to how it currently is on the remote version of the website just in case the main branch is messed up in regard to the merging that has taken place.

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,21 @@
+<style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #32CD32;
+        color: white;
+        text-align: center;
+    }
+</style>
+<footer>
+    <div class="footer">
+        <p class="lead"><em>Copyright</em> &copy; PureFoods</p>
+        <blockquote>2022</blockquote>
+
+        <a href="#" class="position-absolute bottom-0 end-0 p-5">
+            <i class="bi bi-arrow-up-circle h1"></i>
+        </a>
+    </div>
+</footer>
