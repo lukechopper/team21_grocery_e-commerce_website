29ab846bc04d694145e4e992b70f1ebeab46bab7
Sehrish Fatima <200096252@aston.ac.uk>
2022-11-27 00:32:53 +0000

    making about us responsive

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -9,16 +9,17 @@
 <!--all the links for style sheets custom and ready made bootstrap-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <link rel="stylesheet" href="{{asset('css/input.css')}}">
+<link rel="stylesheet" href="{{asset('css/TPdraft.css')}}">
 @endsection
 
 @section('body')
 
 <body>
-
-    <header class="contactheader">
+<div class="container">
+    <header class="about_main">
         <h1 class="heading">About Us</h1>
     </header>
-    <div class="about">
+    <div class="about_main">
         <h2>Know more about us! </h2><br>
         <p>At Pure Foods, we strive to sell products of the highest quality at a low cost! Our goal is to ensure that inclusivity is met by showcasing a wide variety of products to appease customers of all types of backgrounds.</p>
         <p>We ensure: that the process of buying products is as simple and efficient as possible, that are employees work to the best of their ability and at the same time we uphold their rights and develop a safe & inclusive workplace, that the process of buying products is as simple and efficient as possible.
@@ -40,6 +41,7 @@
         <p>Another form of action we have taken includes the purchasing of Fairtrade products. <br>We want to give back to our communities all around the work by using schemes that will benefit both them and us.</p><br><br>
 
     </div>
+</div>
 </body>
 </html>
 </html>
