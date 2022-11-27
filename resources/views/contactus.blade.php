04e353e1cb164bef37e629d8a90db97c89c7b1f9
Azraa Faizar <azraafaizar@gmail.com>
2022-11-27 14:15:37 +0000

    layout and desugn changes

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -115,30 +115,6 @@
             </article>
         </main>
 
-
-
-
-            <div class="row align-items-center my-3">
-                <div class="row">
-                    <div class="col-sm-4 col-lg-6">
-                        <img src="{{asset('chat.png')}}">
-                    </div>
-                    <div class="col-sm-8 col-lg-6">
-                        <h2>We're here to help</h2>
-                        <h3>Contact our support and sales team, We're all ears!</h3><br></br>
-                        <p>At PureFoods we take customer satisfaction seriously. If you are
-                            dissatisfied with you service and experience just get in touch and one of our customer service or technical support agents
-                            will help you. If you would like to learn more about our company values visit About Us:</p>
-                        <form class="aboutbtn">
-                        <button type="submit" class="btn_style" formaction="{{route('aboutus')}}">About Us</button>
-                        </form><br></br>
-                        <p>If you having any issues regaring: <b>Order, Delivery, Payment, Return & Refund, Product & Stock or Account</b>
-                        please email us and we will try our best to resolve your issue promptly.</p><br></br>
-                        <p><small>Please note that our office hours are: <br><br><b>Monday-Friday: 9.00-18.00 <br>Weekends: 10.00-15.00</b></br></br></br></small></p>
-                    </div>
-                </div>
-            </div>
-    </div>
     -->
     </div>
 @endsection
